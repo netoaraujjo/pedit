221aebb0452aa381490223eccadf28cddd007288
Neto Araujo <netoaraujjo@gmail.com>
2015-04-23 10:20:44 -0300

    Comit inicial

---
+++ src/application/Main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package application;
+
+import gui.MainWindow;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Francisco Neto, Luís Guilherme
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MainWindow janelaPrincipal = new MainWindow();
+		janelaPrincipal.setSize(800, 600);
+		janelaPrincipal.setLocationRelativeTo(null);
+		janelaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		janelaPrincipal.setVisible(true);
+	}
+
+}
---
+++ src/gui/MainWindow.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package gui;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Francisco Neto, Luís Guilherme
+ *
+ */
+public class MainWindow extends JFrame {
+	
+	
+	public MainWindow() {
+		super("pEdit");
+	}
+	
+}
