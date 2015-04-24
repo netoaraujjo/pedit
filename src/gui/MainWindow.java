e9d5188b42c0a24592ddba92f306d17da97b3cea
Neto Araujo <netoaraujjo@gmail.com>
2015-04-24 17:29:23 -0300

    menu com atalhos adicionados

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -3,11 +3,13 @@
  */
 package gui;
 
-import javax.swing.JCheckBoxMenuItem;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JFrame;
-import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  * @author Francisco Neto, Luís Guilherme
@@ -22,12 +24,48 @@ public class MainWindow extends JFrame {
 	**************************************/
 	private JMenuBar menuBar;
 	
-	
 	public MainWindow() {
 		super("pEdit");
 		
+		configuraAparencia();
+		
 		menuBar = new MenuBarBuilder().getMenuBar();
 		setJMenuBar(menuBar);
+	}
+	
+	private void configuraAparencia() {
+		String sistema = System.getProperty("os.name");
+		UIManager.LookAndFeelInfo[] looks = UIManager.getInstalledLookAndFeels();
+		List<String> lookNames = new ArrayList<String>();
+		for (UIManager.LookAndFeelInfo look : looks) {
+			lookNames.add(look.getName());
+		}
+		for (String name : lookNames) {
+			System.out.println(name);
+		}
+		
+		/*
+		int lkPos = 0;
+		if (sistema.equals("Linux")) {
+			lkPos = lookNames.indexOf("GTK+");
+		} else if (sistema.contains("Windows")) {
+			lkPos = 
+		} else if (sistema.equals("MacOS")) {
+			
+		} else {
+			lkPos = lookNames.indexOf("Nimbus");
+		}
+		
+		// temporariamente seta nimbus como padrao
+		try {
+			//UIManager.setLookAndFeel(looks[lkPos].getClassName());
+			UIManager.setLookAndFeel(looks[lookNames.indexOf("Nimbus")].getClassName());
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException | UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		
 	}
 	
 }
--- src/gui/MenuBarBuilder.java
+++ src/gui/MenuBarBuilder.java
@@ -1,9 +1,16 @@
 package gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
 import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.KeyStroke;
 
 public class MenuBarBuilder {
 	
@@ -68,36 +75,69 @@ public class MenuBarBuilder {
 	
 	private void configuraMenuArquivo() {
 		novo = new JMenuItem("Novo");
+		novo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_MASK));
 		abrir = new JMenuItem("Abrir");
+		abrir.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_MASK));
 		salvar = new JMenuItem("Salvar");
+		salvar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
 		salvarComo = new JMenuItem("Salvar como...");
+		salvarComo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK | InputEvent.SHIFT_MASK));
 		imprimir = new JMenuItem("Imprimir");
+		imprimir.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, InputEvent.CTRL_MASK));
+		
 		sair = new JMenuItem("Sair");
+		sair.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, InputEvent.CTRL_MASK));
+		sair.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				String mensagem = "Sair do pEdit?";
+				int op = JOptionPane.showConfirmDialog(null, mensagem, "Confirmar Saída", JOptionPane.YES_NO_CANCEL_OPTION);
+				if (op == JOptionPane.YES_OPTION) {
+					System.exit(0);
+				}
+			}
+		});
 		
 		menuArquivo.add(novo);
 		menuArquivo.add(abrir);
+		menuArquivo.addSeparator();
 		menuArquivo.add(salvar);
 		menuArquivo.add(salvarComo);
+		menuArquivo.addSeparator();
 		menuArquivo.add(imprimir);
+		menuArquivo.addSeparator();
 		menuArquivo.add(sair);
 	}
 	
 	private void configuraMenuEditar() {
 		desfazer = new JMenuItem("Desfazer");
+		desfazer.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, InputEvent.CTRL_MASK));
 		refazer = new JMenuItem("Refazer");
+		refazer.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, InputEvent.CTRL_MASK | InputEvent.SHIFT_MASK));
 		recortar = new JMenuItem("Recortar");
+		recortar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.CTRL_MASK));
 		copiar = new JMenuItem("Copiar");
+		copiar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_MASK));
 		colar = new JMenuItem("Colar");
+		colar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.CTRL_MASK));
 		selecionarTudo = new JMenuItem("Selecionar tudo");
+		selecionarTudo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.CTRL_MASK));
 		comentarioLinha = new JMenuItem("Inserir comentário de linha");
+		comentarioLinha.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_MASK | InputEvent.SHIFT_MASK));
 		comentarioBloco = new JMenuItem("Inserir comentário de bloco");
+		comentarioBloco.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_SLASH, InputEvent.CTRL_MASK | InputEvent.SHIFT_MASK));
 		
 		menuEditar.add(desfazer);
 		menuEditar.add(refazer);
+		menuEditar.addSeparator();
 		menuEditar.add(recortar);
 		menuEditar.add(copiar);
 		menuEditar.add(colar);
+		menuEditar.addSeparator();
 		menuEditar.add(selecionarTudo);
+		menuEditar.addSeparator();
 		menuEditar.add(comentarioLinha);
 		menuEditar.add(comentarioBloco);
 	}
