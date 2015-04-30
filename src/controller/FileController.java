7d925d7dbbde921756179772758b92c295d10f3d
Neto Araujo <netoaraujjo@gmail.com>
2015-04-30 19:07:27 -0300

    corrigido problema de criar arquivo

--- src/controller/FileController.java
+++ src/controller/FileController.java
@@ -10,39 +10,41 @@ import java.util.Formatter;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
-import exceptions.ArquivoJaExistenteException;
-
 /**
  * @author Francisco Neto, Luis Guilherme
  *
  */
 public class FileController {
 	
-	public File criaNovoArquivo() throws ArquivoJaExistenteException {
-		JFileChooser fileChooser = new JFileChooser();
-		int result = fileChooser.showSaveDialog(null);
-		if (result != JFileChooser.CANCEL_OPTION) {
-			File file = fileChooser.getSelectedFile();
-			// se o usuario nao inserir a extensao a ide insere
-			String arquivo = file.toString();
-			if (!arquivo.endsWith(".por")) {
-				arquivo += ".por";
-				file = new File(arquivo);
-			}
-			if (file.exists()) {
-				throw new ArquivoJaExistenteException();
-				String msg = "O arquivo já existe. Deseja substitui-lo?";
-				String titulo = "Arquivo ja existente";
-				int op = JOptionPane.showConfirmDialog(null, msg, titulo, JOptionPane.YES_NO_OPTION);
-				if (op == JOptionPane.YES_OPTION) {
-					criaArquivo(file);
-				}
+	public File criaNovoArquivo() {
+		while (true) {
+			JFileChooser fileChooser = new JFileChooser();
+			int result = fileChooser.showSaveDialog(null);
+			
+			if (result == JFileChooser.CANCEL_OPTION) {
+				return null;
 			} else {
-				criaArquivo(file);
+				File file = fileChooser.getSelectedFile();
+				// se o usuario nao inserir a extensao a ide insere
+				String arquivo = file.toString();
+				if (!arquivo.endsWith(".por")) {
+					arquivo += ".por";
+					file = new File(arquivo);
+				}
+				if (file.exists()) {
+					String msg = "O arquivo já existe. Deseja substitui-lo?";
+					String titulo = "Arquivo ja existente";
+					int op = JOptionPane.showConfirmDialog(null, msg, titulo, JOptionPane.YES_NO_OPTION);
+					if (op == JOptionPane.YES_OPTION) {
+						criaArquivo(file);
+						return file;
+					}
+				} else {
+					criaArquivo(file);
+					return file;
+				}
 			}
-			return file;
 		}
-		return null;
 	}
 	
 	private void criaArquivo(File file) {
