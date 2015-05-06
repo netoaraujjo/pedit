5fbc8b3b0503055da8b5097b1360843d1ff187e0
Neto Araujo <netoaraujjo@gmail.com>
2015-05-06 07:44:29 -0300

    classe Main renomeada para MainPEdit

---
+++ src/application/MainPEdit.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package application;
+
+import gui.MainWindow;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Francisco Neto, Lu�s Guilherme
+ *
+ */
+public class MainPEdit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MainWindow janelaPrincipal = new MainWindow();
+		janelaPrincipal.setSize(800, 600);
+		janelaPrincipal.setLocationRelativeTo(null);
+		janelaPrincipal.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		janelaPrincipal.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		janelaPrincipal.setVisible(true);
+	}
+
+}
